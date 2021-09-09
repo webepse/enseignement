7f83289817be83407e8cea6379ebb187a4c0ebd6
Berti <berti@epse.be>
2021-09-09 09:17:12 +0200

    init search.php

---
+++ search.php
@@ -0,0 +1,32 @@
+<?php
+    require "connexion.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Enseignement</title>
+</head>
+<body>
+    <header>
+        <h1>Enseignement</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php?degre=all">Tous les degrés</a></li>
+            <li><a href="index.php?degre=1">1er degré</a></li>
+            <li><a href="index.php?degre=2">2ème degré</a></li>
+            <li><a href="index.php?degre=3">3ème degré</a></li>
+            <li><a href="search.php">Recherche</a></li>
+        </ul>
+    </nav>
+    <section>
+       <h1>Recherche</h1>
+    </section>
+    <footer></footer>
+</body>
+</html>
